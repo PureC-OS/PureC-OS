a178b48f5929da304b14b723840a3e40657dfe2a
pablaofficeal <pavlogorosko56@gmail.com>
2026-09-16 23:03:14 +0200

    update

--- src/programs/help/help.c
+++ src/programs/help/help.c
@@ -12,6 +12,7 @@ int help_main(void){
     pc_write("  install | setup | update | mkfs.fat32\n");
     pc_write("  uname | about | systeminfo | htop | font | snake | tetris | files | gui-demo\n");
     pc_write("  mouse | debug | battery | reboot | poweroff | shutdown | halt\n");
+    pc_write("  devmgr\n");
     return 0;
 }
 
