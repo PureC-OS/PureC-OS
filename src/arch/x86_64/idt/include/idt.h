e697f863279e6c80a744b51c1c16003cd243a5c5
pablaofficeal <pavlogorosko56@gmail.com>
2026-09-17 16:07:08 +0200

    update

--- src/arch/x86_64/idt/include/idt.h
+++ src/arch/x86_64/idt/include/idt.h
@@ -3,3 +3,7 @@
 
 void idt_init(void);
 void idt_set_gate(int n, uint64_t handler, uint8_t flags);
+void idt_set_irq_handler(uint8_t irq, void *fn, void *ctx);
+void idt_clear_irq_handler(uint8_t irq);
+void idt_unmask_irq(uint8_t irq);
+void idt_mask_irq(uint8_t irq);
