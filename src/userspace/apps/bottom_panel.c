bf31887cfaabd6ea86bafe5653315d6ddec05845
pablaofficeal <pavlogorosko56@gmail.com>
2026-09-17 19:25:16 +0200

    update

--- src/userspace/apps/bottom_panel.c
+++ src/userspace/apps/bottom_panel.c
@@ -4,6 +4,8 @@
 #include "../display.h"
 #include "../personalization.h"
 #include "../window_manager.h"
+#include "../../kernel/process/process.h"
+#include "../syscall.h"
 #include "../../kernel/syscall/syscall.h"
 #include "../../lib/string.h"
 
